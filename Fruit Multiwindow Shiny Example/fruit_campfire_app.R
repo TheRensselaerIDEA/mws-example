f0022946ed49251a212400310e50c2072aaf0068
Hannah De los Santos <hdelossantos653@gmail.com>
2018-07-17 15:08:25 -0400

    uploading all necessary multiwindow shiny files

---
+++ Fruit Multiwindow Shiny Example/fruit_campfire_app.R
@@ -0,0 +1,191 @@
+# Larger Multi-Window Shiny Example
+# By Hannah De los Santos
+# Originated on: 7/16/18
+
+# Description: A larger example, with plots, of multi-window shiny,
+# based on Nick's implementation. Meant to also be an example for 
+# a good multi-window shiny code workflow.
+
+# Data is from the USDA website:
+# https://www.ers.usda.gov/data-products/fruit-and-tree-nut-data/fruit-and-tree-nut-yearbook-tables/#General
+
+# Note: this is the script you should run.
+
+# set up ----
+
+# set working directory, if not already set - only works in RStudio (with rstudioapi)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# script with campfire functionality for floors and wall
+source("fruit_campfire_lib.R")
+
+# running app ----
+
+campfireApp(
+  
+  # controller: where selections and possibly shiny app information reside
+  controller = fluidPage(tweaks,
+                 fluidRow(column(width = 12, 
+                   list(h3("Controller: US Fruit and Nut Data Explorer"), 
+                        # checkbox slector for which fruits to look at
+                    tags$div(align = 'left', 
+                             class = 'multicol', 
+                             checkboxGroupInput(inputId = "fruit",
+                                                label = "Fruits/Nuts to Examine:",
+                                                choices = c("Grapefruits", "Lemons",
+                                                            "Oranges", "Tangerines",
+                                                            "Apples", "Avocados",
+                                                            "Cherries", "Grapes",
+                                                            "Nectarines", "Olives",
+                                                            "Peaches", "Pears",
+                                                            "Pineapples", "Plums and prunes",
+                                                            "Cranberries", "Strawberries",
+                                                            "Blueberries", "Almonds",
+                                                            "Pecans", "Walnuts",
+                                                            "Other fruit and nuts"),
+                                                inline = F)
+                                ) 
+                         ))),
+                 # slider input for which years to choose
+                 fluidRow(column(width = 12, 
+                                 tags$div(align = 'left',
+                                  sliderInput("years", "Years to Examine:",
+                                        min = 1980,
+                                        max = 2016,
+                                        value = c(1989,2016),
+                                        sep = "")))),
+                 # select which dataset to look at
+                 fluidRow(column(width = 12, 
+                                 tags$div(align = 'left',
+                                          selectInput("which_dat",
+                                                      "Dataset to View:",
+                                                      choices = c("Average Retail Value" = "retail",
+                                                                  "Cash Receipts" = "cash"))))),
+                 # action button to update pictures
+                 fluidRow(column(width = 12, 
+                                 tags$div(align = 'left',
+                                  actionButton("go","Update"))))
+  ),
+  
+  wall = div(
+    plotOutput("wall", height = "800px"),
+    verbatimTextOutput("wallText"),
+    style="background: rgb(255, 255, 255);"
+  ),
+  
+  floor = div(
+    plotOutput("floor", height = "800px"),
+    style="background: rgb(255, 255, 255);"
+  ),
+  
+  monitor = div(
+    plotOutput("monitor", height = "800px"),
+    style="background: rgb(255, 255, 255);"
+  ),
+  
+  # render output here ----
+  
+  serverFunct = function(serverValues, output) {
+    # this is where we access the serverValues passed in the library script
+    # only plots should be rendered here -- little to no computation.
+    
+    # on the monitor, we plot an emoji of a random selected fruit, if available
+    output$monitor <- renderPlot({ 
+      if (length(serverValues$fruit) == 0){
+        ggplot() + 
+          geom_emoji("fork_and_knife", color="grey", size = 80) + 
+          theme_void() 
+      } else {
+        choose_fruit <- sample(serverValues$fruit,1)
+        if (choose_fruit == "Grapes") {
+          ggplot() + 
+            geom_emoji("grapes", color=color_map["Grapes"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Pears") {
+          ggplot() + 
+            geom_emoji("pear", color=color_map["Pears"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Peaches") {
+          ggplot() + 
+            geom_emoji("peach", color=color_map["Peaches"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Tangerines") {
+          ggplot() + 
+            geom_emoji("tangerine", color=color_map["Tangerines"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Strawberries") {
+          ggplot() + 
+            geom_emoji("strawberry", color=color_map["Strawberries"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Pineapples") {
+          ggplot() + 
+            geom_emoji("pineapple", color=color_map["Pineapples"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Cherries") {
+          ggplot() + 
+            geom_emoji("cherries", color=color_map["Cherries"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Avocados") {
+          ggplot() + 
+            geom_emoji("avocado", color=color_map["Avocados"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Apples") {
+          ggplot() + 
+            geom_emoji("apple", color=color_map["Apples"], size = 80) + 
+            theme_void() 
+        } else if (choose_fruit == "Lemons") {
+          ggplot() + 
+            geom_emoji("lemon", color=color_map["Lemons"], size = 80) + 
+            theme_void() 
+        } else {
+          ggplot() + 
+            geom_emoji("fork_and_knife", color="grey", size = 80) + 
+            theme_void() 
+        }
+      } 
+      
+      
+    })
+    
+    # on the wall, we render a grouped bar plot of value over time
+    output$wall <- renderPlot({ 
+      if (!is.null(serverValues$go)){
+        suppressWarnings( 
+          ggplot(serverValues$fruit_bar, aes_string("Year", paste0(serverValues$which_dat, ".value"))) +
+            geom_bar(aes(fill = Fruit), position = "dodge", stat="identity") +
+            scale_fill_manual(values = serverValues$color_map[serverValues$fruit]) +
+            NULL
+        )
+      }
+    })
+    # also on the wall we render which fruit don't appear in the selected dataset
+    output$wallText <- renderText({
+      if (!is.null(serverValues$go)){
+      return(paste0("The following fruit are not in this dataset: ", 
+                    paste(serverValues$bad_fruit, collapse = " ")))
+      }
+    })
+    
+    # on the floor, we render a pie chart of total value over years
+    output$floor <- renderPlot({ 
+      suppressWarnings(
+        if (!is.null(serverValues$go)){
+          ggplot(serverValues$fruit_pie, aes(x = "", y = Total.Value, fill = Fruit)) +
+            geom_bar(width = 1, stat = "identity")  +
+            coord_polar(theta = "y", start = 0) +
+            scale_fill_manual(values = serverValues$color_map[serverValues$fruit]) +
+            scale_y_continuous(breaks = round(cumsum(rev(serverValues$fruit_pie$Total.Value)), serverValues$tot_val)) +
+            theme(#axis.title.x=element_blank(),
+                  # axis.text.x=element_blank(),
+                  # axis.ticks.x=element_blank(),
+                  axis.title.y=element_blank(),
+                  axis.text.y=element_blank(),
+                  axis.ticks.y=element_blank()
+                  ) +
+            NULL
+        }
+      )
+    })
+  }
+  
+  )
---
+++ Fruit Multiwindow Shiny Example/fruit_campfire_lib.R
@@ -0,0 +1,180 @@
+# Fruit Campfire Library
+# by Hannah De los Santos
+# Originated on: 7/17/18
+
+# load libraries, data, variables, and misc functions ----
+
+# libraries
+library(shiny)
+library(ggplot2)
+library(reshape2)
+library(emojifont)
+
+# data
+# data source: https://www.ers.usda.gov/data-products/fruit-and-tree-nut-data/fruit-and-tree-nut-yearbook-tables/#General
+retail_fruit <- read.csv("US_Fruit_Avg_Retail_Value_1989_to_2016.csv")
+rownames(retail_fruit) <- retail_fruit$Fruit
+cash_fruit <- read.csv("US_Fruit_Cash_Receipts_in_1000s_1980_to_2016.csv")
+rownames(cash_fruit) <- cash_fruit$Fruit
+
+# variables
+
+color_map <- c("Grapefruits" = "#ea5744", "Lemons" ="#fcf011",
+               "Oranges" = "#fc9210", "Tangerines" = "#f7a33d",
+               "Apples" = "#ff0000", "Avocados" = "#2da844",
+               "Cherries" = "#a80528", "Grapes" = "#6404a0",
+               "Nectarines" = "#f7a222", "Olives" = "#191818",
+               "Peaches" = "#ef9267", "Pears" = "#b1ef66",
+               "Pineapples" = "#fff963", "Plums and prunes" = "#d11be5",
+               "Cranberries" = "#751313", "Strawberries" = "#e20047",
+               "Blueberries" = "#0010c4", "Almonds" = "#ce9a40",
+               "Pecans" = "#fcb250", "Walnuts" = "#331d00",
+               "Other fruit and nuts" = "#5e5b57")
+
+# functions
+
+# tweaks, a list object to set up multicols for checkboxGroupInput
+# source: https://stackoverflow.com/questions/29738975/how-to-align-a-group-of-checkboxgroupinput-in-r-shiny
+tweaks <- list(tags$head(tags$style(HTML("
+                                 .multicol { 
+                                   height: 150px;
+                                   -webkit-column-count: 5; /* Chrome, Safari, Opera */ 
+                                   -moz-column-count: 5;    /* Firefox */ 
+                                   column-count: 5; 
+                                   -moz-column-fill: auto;
+                                   -column-fill: auto;
+                                 } 
+                                 "))
+                         )
+               )
+
+# adjust campfire edits ----
+
+# In this function, this is where you do the bulk of your
+# processing: subsetting data to be passed to visualizations,
+# calculations, etc.
+
+campfireApp = function(controller = NA, wall = NA, floor = NA, monitor=NA, serverFunct = NA) {
+  ui <- campfireUI(controller, wall, floor, monitor)
+  
+  # our reactive values that we will pass to our rendering
+  serverValues = reactiveValues()
+  
+  # function where the computation (server) happens
+  campfire_server <- shinyServer(function(input, output) {
+    
+    observeEvent(input$go, {
+      # reassign everything from the input to the serverValues
+      # from now on, only reference input values with serverValues
+      for (inputId in names(input)) {
+        serverValues[[inputId]] <- input[[inputId]]
+      }
+      
+      # subset the data for the years and fruits we care about
+      # column names start with X
+      begin <- serverValues$years[1]
+      end <- serverValues$years[2]
+      
+      # subset the df we care about
+      if (serverValues$which_dat == "cash"){
+        sub_fruit <- cash_fruit[serverValues$fruit,c("Fruit",paste0("X",c(begin:end)))]
+        
+        # no bad fruit here!
+        bad_fruit <- c()
+      } else {
+        # not all fruit appear in the retail dataset
+        fruit_logical <- serverValues$fruit %in% rownames(retail_fruit)
+        # fruit in retail
+        good_fruit <- serverValues$fruit[fruit_logical]
+        # fruit out of retail
+        bad_fruit <- serverValues$fruit[!fruit_logical]
+        
+        # also this dataset has nothing before 1989
+        if (begin < 1989) {
+          begin <- 1989
+        }
+        
+        sub_fruit <- retail_fruit[good_fruit,c("Fruit",paste0("X",c(begin:end)))]
+      }
+      
+      # make the bar plot data frame (value for each year):
+      
+      # get into long format, using fruits as the id
+      fruit_bar <- melt(sub_fruit, id.vars = "Fruit")
+      colnames(fruit_bar)[2:3] <- c("Year", paste0(serverValues$which_dat, ".value"))
+      # remove all X's from the time column (still a string is ok)
+      fruit_bar$Year <- gsub("X","", fruit_bar$Year)
+      
+      # make the pie chart dataframe (sum total value for all years):
+      fruit_pie <- data.frame(matrix(0,nrow(sub_fruit), 2))
+      colnames(fruit_pie) <- c("Fruit", "Total.Value")
+      fruit_pie$Fruit <- sub_fruit$Fruit
+      fruit_pie$Total.Value <- as.numeric(rowSums(sub_fruit[,-1], na.rm = T))
+      fruit_pie <- fruit_pie[order(fruit_pie$Fruit),]
+      tot_val <- sum(fruit_pie$Total.Value)
+      
+      
+      # add this to serverValues, which we will use to create the plot
+      # if you want to access a value when plotting, add it to serverValues
+      serverValues[["fruit_bar"]] <- fruit_bar
+      serverValues[["bad_fruit"]] <- bad_fruit
+      serverValues[["fruit_pie"]] <- fruit_pie
+      serverValues[["tot_val"]] <- tot_val
+      serverValues[["color_map"]] <- color_map
+      
+    })
+    serverFunct(serverValues, output)
+    
+  })
+  
+  options(shiny.port = 5480)
+  shinyApp(ui, server = campfire_server)
+}
+
+# campfire ui ----
+
+# This is the section that controls what the user sees
+# when they load the app. No need to edit anything in 
+# this section.
+
+campfireUI = function(controller, wall, floor, monitor) {
+  ui <- shinyUI(bootstrapPage(
+    HTML('<script type="text/javascript">
+         $(function() {
+         $("div.Window").hide(); 
+         var tokens = window.location.href.split("?");
+         if (tokens.length > 1) {
+         var shown_window = tokens[1];
+         $("div."+shown_window).show();
+         } else {
+         $("div.WindowSelector").show();
+         }
+         });
+         </script>'),
+		div(class="WindowSelector Window",
+		    HTML('<h2><a href="?Controller">Controller</a></h2>'),
+		    HTML('<h2><a href="?Wall">Wall</a></h2>'),
+		    HTML('<h2><a href="?Floor">Floor</a></h2>'),
+		    HTML('<h2><a href="?Monitor">External Monitor</a></h2>'),
+		    style='position: absolute; 
+		    top: 50%; left: 50%; 
+		    margin-right: -50%; 
+		    transform: translate(-50%, -50%)'
+		),
+		div(class="Controller Window",
+		    controller
+		),
+		div(class="Wall Window",
+		    wall 
+		),
+		div(class="Floor Window",
+		    floor
+		),
+		div(class="Monitor Window",
+		    monitor
+		)
+		
+	))
+
+	return(ui)
+}
